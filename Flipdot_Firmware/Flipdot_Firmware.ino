c088bbc0ea1e0e8f81f6bce83728e6fe26de369c
Ralf <ralf_stoffels@gmx.de>
2016-08-21 14:04:33 +0200

    neue version

--- Flipdot_Firmware/Flipdot_Firmware.ino
+++ Flipdot_Firmware/Flipdot_Firmware.ino
@@ -2,22 +2,32 @@
 // Flipdot_Firmware
 // CC-BY-SA-NC    8/2016 C-Hack (Ralf@Surasto.de)
 //
-// Hauptprogramm für Arduino
-// Arduino wartet auf Befehle über die serielle Schnittstelle und steuert 
-// entsprechend die Flipdot-Anzeige
+// This main program runs on the Arduino and waits for commands via the RS232 
+// For simplicity the commands are handed over as an ascii string so that
+// they can be easily created by scripts
 //
-// Datenfomat:
-//    <Byte 1>                Befehl
-//    <Byte 2> + <Byte 3>     16 bit x-Koordinate
-//    <Byte 4>                 8 bit y-Koordinate
-//    <Byte 5...n>            Textstring  (Ende mit 0x00)
+// Command format
+//   <Command>,<Color>,<x>,<y>,<....string....>\
 //
-//    Befehle:
-//      0x01   Print String SMALL
-//      0x02   Print String MEDIUM
-//      0x03   Print String LARGE
-//      0x04   Clear Screen Yellow
-//      0x05   Clear Screen Black
+//   Commands:
+//     C  Clear Screen
+//     Q  Quick Clear (changes only those pixels which have to be flipped)
+//     S  Print in small font
+//     M  Print in medium Font
+//     L  Print in large Font
+//     H  Draw a horizontal line
+//   Color:
+//     B  Black
+//     Y  Yellow
+//   X,Y:
+//     Required for all Print commands
+//     Only Y is required for the horizontal line command "H"
+//   String:
+//     Contains the characters to be printed 
+//   "\": 
+//     The command lines is terminated by the "\" character
+//     It gets evaluated after reception of that character
+//             
 ////////////////////////////////////////////////////////////////////////////
 
 
@@ -26,6 +36,9 @@
 #define OFF -1
 
 int i,j;
+int inByte;
+int commandLine[100];
+int commandLength=0;
 
 void setup() {
   
@@ -35,6 +48,58 @@ void setup() {
 }
 
 void loop() {
+  int c, color;
+  int cmd, cmdPtr;
+  int xVal, yVal;
+  String x,y;
+  String outputString;
+  
+  if (Serial.available() > 0) {
+    c = Serial.read();
+    if (commandLength<100) commandLine[commandLength++] = c;
+    else {
+      commandLength = 0;
+      Serial.print("?");
+    }
+    
+    // ==== If command string is complete... =======
+    if (c=='\\') {
+      cmd = commandLine[0];
+      color = commandLine[2];
+      cmdPtr=4;
+      x="";
+      while ((cmdPtr<=commandLength) && (commandLine[cmdPtr]!=',')) {
+        x = x + commandLine[cmdPtr];
+        cmdPtr++;
+        xVal = atoi(x);
+      }
+      cmdPtr++;
+      while ((cmdPtr<=commandLength) && (commandLine[cmdPtr]!=',')) {
+        y = y + commandLine[cmdPtr];
+        cmdPtr++;
+        yVal = atoi(y);
+      }
+      cmdPtr++;
+      while (cmdPtr<=commandLength) {
+        outputString = outputString + commandLine[cmdPtr];
+        cmdPtr++;
+      }
+    }
+
+    // ======= Execute the respective command ========
+    switch (cmd) {
+      case 'C':  break;
+      case 'Q':  break;
+      
+    }
+  }
+}
+
+
+//===========================
+// For debugging only
+//===========================
+void testPanel() {
   
    writePanel(-1);
    //delay(500);
